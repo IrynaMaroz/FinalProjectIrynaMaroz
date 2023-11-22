71120cb52b61550521001d1886adc3939954f678
IrynaMaroz <maroz.iryna@gmail.com>
2023-11-22 09:51:00 +0200

    testst corrected

--- Tests/PurchasingScenario.cs
+++ Tests/PurchasingScenario.cs
@@ -51,25 +51,34 @@ public class PiguLtLoginTest
     [Test, Order (2)]
     public void HamburgerMenuTesting()
     {
+        driver.Navigate().GoToUrl("https://pigu.lt/lt/");
+        System.Threading.Thread.Sleep(1000);
+        IWebElement closeCookies = driver.FindElement(By.XPath("//*[@id='cookie_block']/div/div/div[2]/div[2]/button[3]"));
+        closeCookies.Click();
+        System.Threading.Thread.Sleep(2000);
         IWebElement hamburgerMenuIcon = driver.FindElement(By.XPath("//*[@id='menuBurger']"));
-        hamburgerMenuIcon.Click();
+        Actions actions = new Actions(driver);
+        actions.MoveToElement(hamburgerMenuIcon);
+        actions.Perform();
 
+        System.Threading.Thread.Sleep(1000);
         IWebElement kvepalaiKosmetikaCategory = driver.FindElement(By.XPath("//*[@id='department-82']/a/span"));
         kvepalaiKosmetikaCategory.Click();
 
-        IWebElement kvepalaiMoterimsSubcategory = driver.FindElement(By.XPath("//*[@id='department-82']/div/div/div[1]/ul[1]/li[1]/a"));
-        kvepalaiMoterimsSubcategory.Click();
-
         System.Threading.Thread.Sleep(5000);
-        string expectedSearchResult = "Originalūs kvepalai moterims";
-        IWebElement searchResult = driver.FindElement(By.XPath("//*[@id='catalogSeoText']/div/div[1]/div/p/strong[1]"));
+        string expectedSearchResult = "Kvepalai";
+        IWebElement searchResult = driver.FindElement(By.XPath("//*[@id='productsCategoryBranch']/div[2]/div/div/div[1]/a/p"));
         Assert.That(expectedSearchResult, Is.EqualTo(searchResult.Text));
     }
 
     [Test, Order (3)]
     public void AddKvepalaiMoterimsToCartPerSearch()
     {
-       
+        driver.Navigate().GoToUrl("https://pigu.lt/lt/");
+        System.Threading.Thread.Sleep(1000);
+        IWebElement closeCookies = driver.FindElement(By.XPath("//*[@id='cookie_block']/div/div/div[2]/div[2]/button[3]"));
+        closeCookies.Click();
+        System.Threading.Thread.Sleep(2000);
         IWebElement searchInput = driver.FindElement(By.Id("searchInput"));
         searchInput.SendKeys("kvepalai moterims");
         searchInput.SendKeys(Keys.Enter);
@@ -77,40 +86,88 @@ public class PiguLtLoginTest
         IWebElement firstResult = driver.FindElement(By.XPath("//*[@id='_0productBlock1039724']/div/div/div[3]/a/img"));
         firstResult.Click();
 
-        IWebElement addToCartButton = driver.FindElement(By.XPath("//*[@id='product-sidebar']/div[2]/div[6]/div/div[1]"));
+        System.Threading.Thread.Sleep(2000);
+        IWebElement addToCartButton = driver.FindElement(By.XPath("//*[@id='product-sidebar']/div[1]/div[6]/div/div[1]"));
         addToCartButton.Click();
 
         System.Threading.Thread.Sleep(5000);
         string expectedAddedToCart = "Prekė įtraukta į krepšelį";
-        IWebElement addedToCart = driver.FindElement(By.XPath("//*[@id=\"add-to-cart-modal-header\"]/div"));
+        IWebElement addedToCart = driver.FindElement(By.XPath("//*[@id='add-to-cart-modal-header']/div"));
         Assert.That(expectedAddedToCart, Is.EqualTo(addedToCart.Text));
     }
 
     [Test, Order (4)]
-    public void ShoppingCartPurchaseWithEmptyBuyerInformation()
+    public void ShoppingCartContinueButton()
     {
+        driver.Navigate().GoToUrl("https://pigu.lt/lt/");
+        System.Threading.Thread.Sleep(1000);
+        IWebElement closeCookies = driver.FindElement(By.XPath("//*[@id='cookie_block']/div/div/div[2]/div[2]/button[3]"));
+        closeCookies.Click();
+        System.Threading.Thread.Sleep(2000);
+        IWebElement loginButton = driver.FindElement(By.XPath("//*[@id='headeMenu']/li[1]/a/i"));
+        Actions actions = new Actions(driver);
+        actions.MoveToElement(loginButton);
+        actions.Perform();
+        IWebElement prisijungtiButton = driver.FindElement(By.XPath("//*[@id='headeMenu']/li[1]/div/div/div[2]/a[1]"));
+        prisijungtiButton.Click();
+        IWebElement usernameInput = driver.FindElement(By.XPath("//*[@id='loginModal']/div[1]/div[1]/form/div[3]/input"));
+        IWebElement passwordInput = driver.FindElement(By.XPath("//*[@id='passwordCont']/input"));
 
-        driver.FindElement(By.XPath("//*[@id='buy']")).Click(); //press buy button for past test performint
+        usernameInput.SendKeys("testastest40@gmail.com");
+        passwordInput.SendKeys("testas");
+        IWebElement searchInput = driver.FindElement(By.Id("searchInput"));
+        searchInput.SendKeys("kvepalai moterims");
+        searchInput.SendKeys(Keys.Enter);
 
-        
-        driver.FindElement(By.XPath("//*[@id='form154416133']/div[2]/div[4]/div[2]/div[2]/div[2]/div/div/button")).Click();
+        IWebElement firstResult = driver.FindElement(By.XPath("//*[@id='_0productBlock1039724']/div/div/div[3]/a/img"));
+        firstResult.Click();
 
-        driver.FindElement(By.XPath("//*[@id='create_customer']/div[3]/div[3]/div/div/button")).Click();
-        System.Threading.Thread.Sleep(150);
-        IWebElement inputName = driver.FindElement(By.XPath("//*[@id='name']"));
-        string expectedBorderColor = "rgb(255, 0, 0)";
-        string actualBorderColor = inputName.GetCssValue("border-color");    
-        Assert.That(actualBorderColor, Is.EqualTo(expectedBorderColor));
+        System.Threading.Thread.Sleep(2000);
+        IWebElement addToCartButton = driver.FindElement(By.XPath("//*[@id='product-sidebar']/div[1]/div[6]/div/div[1]"));
+        addToCartButton.Click();
+        System.Threading.Thread.Sleep(2000);
+        driver.FindElement(By.XPath("//*[@id='buy']")).Click(); //press buy button 
+        System.Threading.Thread.Sleep(2000);
+        string expectedResult = "VIENETO KAINA";
+        IWebElement actualResult = driver.FindElement(By.XPath("//*[@id='form154739450']/div[2]/table/tbody/tr[1]/th[2]"));
+        Assert.That(expectedResult, Is.EqualTo(actualResult.Text));
     }
 
     [Test, Order (5)]
     public void DeleteItemsFromShoppingCart()
     {
-        IWebElement returnToShoppingCart = driver.FindElement(By.XPath("//*[@id='cartInHeader']/li/a/i"));
+        driver.Navigate().GoToUrl("https://pigu.lt/lt/");
+        System.Threading.Thread.Sleep(1000);
+        IWebElement closeCookies = driver.FindElement(By.XPath("//*[@id='cookie_block']/div/div/div[2]/div[2]/button[3]"));
+        closeCookies.Click();
+        System.Threading.Thread.Sleep(2000);
+        IWebElement loginButton = driver.FindElement(By.XPath("//*[@id='headeMenu']/li[1]/a/i"));
+        Actions actions = new Actions(driver);
+        actions.MoveToElement(loginButton);
+        actions.Perform();
+        IWebElement prisijungtiButton = driver.FindElement(By.XPath("//*[@id='headeMenu']/li[1]/div/div/div[2]/a[1]"));
+        prisijungtiButton.Click();
+        IWebElement usernameInput = driver.FindElement(By.XPath("//*[@id='loginModal']/div[1]/div[1]/form/div[3]/input"));
+        IWebElement passwordInput = driver.FindElement(By.XPath("//*[@id='passwordCont']/input"));
+
+        usernameInput.SendKeys("testastest40@gmail.com");
+        passwordInput.SendKeys("testas");
+        IWebElement searchInput = driver.FindElement(By.Id("searchInput"));
+        searchInput.SendKeys("kvepalai moterims");
+        searchInput.SendKeys(Keys.Enter);
+
+        IWebElement firstResult = driver.FindElement(By.XPath("//*[@id='_0productBlock1039724']/div/div/div[3]/a/img"));
+        firstResult.Click();
+
+        System.Threading.Thread.Sleep(2000);
+        IWebElement addToCartButton = driver.FindElement(By.XPath("//*[@id='product-sidebar']/div[1]/div[6]/div/div[1]"));
+        addToCartButton.Click();
+        System.Threading.Thread.Sleep(2000);
+        driver.FindElement(By.XPath("//*[@id='buy']")).Click(); //press buy button 
         //System.Threading.Thread.Sleep(2000);
         WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
         wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@id='cartWidget']/a")));
-        IWebElement deleteItemFromShoppingCart = driver.FindElement(By.XPath("//*[@id='productSliceBlock435675408']/td[6]/div/div[2]"));
+        IWebElement deleteItemFromShoppingCart = driver.FindElement(By.XPath("//*[@id='productSliceBlock436384410']/td[6]/div/div"));
         string expectedItemDeleted = "Jūsų prekių krepšelis tuščias";
         IWebElement deletedItem = driver.FindElement(By.XPath("//*[@id='mainBlock']/div/div[1]/div/div"));
         Assert.That(expectedItemDeleted, Is.EqualTo(deletedItem.Text));
